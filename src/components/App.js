f28f4fe13cbfe0448079da076513bb06ff4ec5d9
kcbelford <kcbelford@hotmail.com>
2021-03-17 10:21:03 -0700

    add state to toggle button color

--- src/components/App.js
+++ src/components/App.js
@@ -4,10 +4,10 @@ import LanguageContext from '../contexts/LanguageContext';
 import ColorContext from '../contexts/ColorContext';
 
 class App extends React.Component {
-  state= { language: 'english' };
+  state= { language: 'english', color: 'blue' };
 
-  onLanguageChange = language => {
-    this.setState({ language: language });
+  onLanguageChange = (language, color) => {
+    this.setState({ language: language, color: color });
   };
 
   render() {
@@ -15,10 +15,10 @@ class App extends React.Component {
         <div className="ui container">
           <div>
             Select a language:
-            <i className="flag us" onClick={() => this.onLanguageChange('english')} />
-            <i className="flag nl" onClick={() => this.onLanguageChange('dutch')} />
+            <i className="flag us" onClick={() => this.onLanguageChange('english', 'blue')} />
+            <i className="flag nl" onClick={() => this.onLanguageChange('dutch', 'red')} />
           </div>
-          <ColorContext.Provider value="red">
+          <ColorContext.Provider value={this.state.color}>
             <LanguageContext.Provider value={this.state.language}>
               <UserCreate />
             </LanguageContext.Provider>
